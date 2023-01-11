943c5806bc4e9c260079b4235e7460a120900496
EmericW <emericwarnez@bitsoflove.be>
2023-01-11 20:51:27 +0100

    feat(server): add prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './services/prisma.service';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
